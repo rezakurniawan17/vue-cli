16be355d8dff88d9374806ceb8b28a85a6e317a2
Reza Kurniawan <rezaa.kurniawan17@gmail.com>
2020-09-27 01:55:34 +0700

    coba vue-js di github

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <app-header></app-header>
+  <app-ninjas></app-ninjas>
+  <app-footer></app-footer>
+</div>
+</template>
+
+<script>
+// Import Components
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Ninjas from './components/Ninjas.vue'
+
+export default {
+  // Membuat components
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas
+  },
+
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
