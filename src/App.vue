3e0d84240738cf7db92861a0f998b6e13b3a0a28
Reza Kurniawan <rezaa.kurniawan17@gmail.com>
2020-09-28 09:52:11 +0700

    Slots

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-
+    <form-helper>
+      <div slot="form-header">
+        <h3>This is the title of the form</h3>
+        <p>Information of the form</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required>
+        <input type="password" placeholder="password" required>
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
+    </form-helper>
   </div>
 </template>
 
@@ -8,9 +20,19 @@
 import formHelper from './components/formHelper.vue'
 
 export default {
-  component: {
-    'form-helper': formHelper
-  }
+  components: {
+    'formHelper': formHelper
+  },
+  data() {
+    return {
+      title: 'I am a dynamic Slot Title'
+    }
+  },
+  methods: {
+    handleSubmit: function() {
+      alert('Thanks for submitting');
+    }
+  },
 }
 </script>
 
