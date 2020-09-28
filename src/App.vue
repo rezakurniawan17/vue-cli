fcb30ed594e294b04e84a0ab13074273d7b4093c
Reza Kurniawan <rezaa.kurniawan17@gmail.com>
2020-09-28 09:27:20 +0700

    Slots

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,15 @@
 <template>
-<div>
-  <app-header v-bind:title="title" v-on:changeTitle='updateTitle($event)'></app-header>
-  <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
-  <app-footer v-bind:title="title"></app-footer>
-</div>
+  <div>
+
+  </div>
 </template>
 
 <script>
-// Import Components
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
-import Ninjas from './components/Ninjas.vue'
+import formHelper from './components/formHelper.vue'
 
 export default {
-  // Membuat components
-  components: {
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-ninjas': Ninjas
-  },
-
-  data() {
-    return {
-      ninjas: [
-        {name: 'Ryu', specialty: 'Vue Components', show: false},
-        {name: 'Crystal', specialty: 'HTML Wizardry', show: false},
-        {name: 'Hitoshi', specialty: 'Click Events', show: false},
-        {name: 'Tango', specialty: 'Conditionals', show: false},
-        {name: 'Kami', specialty: 'Webpack', show: false},
-        {name: 'Yoshi', specialty: 'Data Diggin', show: false}
-      ],
-      title: 'Vue Ninjass',
-    }
-  },
-  methods: {
-    updateTitle: function(updatedTitle) {
-      this.title = updatedTitle
-    }
+  component: {
+    'form-helper': formHelper
   }
 }
 </script>
---
+++ src/components/formHelper.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>i am the form helper</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<style>
+
+</style>
