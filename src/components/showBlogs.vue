b617270de7eeb321277e979cf6b25fd0bb48710a
Reza Kurniawan <rezaa.kurniawan17@gmail.com>
2020-10-14 14:06:29 +0700

    Menambahkan playlist 33 GET Request

---
+++ src/components/showBlogs.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <div v-bind:key="blog" v-for="blog in blogs" class="single-blog">
+      <h2>{{blog.title}}</h2>
+      <p>{{blog.body}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ data() {
+   return {
+     blogs: []
+   }
+ },
+ methods: {
+   
+ },
+ created() {
+  //  GET REQUEST
+   this.$http.get('https://jsonplaceholder.typicode.com/posts')
+   .then(data => this.blogs = data.body.slice(0,10))
+ },
+}
+</script>
+
+<style>
+#show-blogs{
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.single-blog{
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border;
+  background: #eeeeee;
+}
+</style>
